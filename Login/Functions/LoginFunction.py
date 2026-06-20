caf6b355ec34ec59ea574987d7bbfb87eb2b1605
Asgard <al222510064@gmail.com>
2026-06-19 19:35:51 -0600

    add: Documentacion y argon2

--- Login/Functions/LoginFunction.py
+++ Login/Functions/LoginFunction.py
@@ -2,3 +2,14 @@ class LoginFunction:
     def validar_datos(self, value:str):
         if not value.strip():
             return True
+        else:
+            return False
+
+    def obtener_datos(self, usuario:str, contraseña:str):
+        if self.validar_datos(usuario):
+            return {"mensaje": "El usuario es invalido"}
+        elif self.validar_datos(contraseña):
+            return {"mensaje": "La contraseña es invalido"}
+        else:
+            return {"mesaje": "Datos no encontrados"}
+
