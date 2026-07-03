567e8ef69c88b92f40814bc38a6867f68bf30a60
Asgard <al222510064@gmail.com>
2026-07-03 02:06:21 -0600

    Se genero un la cenexion a la base de datos

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from Login.LoginController import router as login_router
-from Documentacion.EjemploApi import router as doc_router
-from Usuario.UsuarioController import router as us_router
+from dotenv import load_dotenv
+import pymysql
+from BaseDatos.MySqlManager import MySqlManager
 import sys
 import os.path
+import os
 
 if __package__ is None and not getattr(sys, 'frozen', False):
     # Obtiene la ruta absoluta de este main.py
     path = os.path.realpath(os.path.abspath(__file__))
     # Agrega la carpeta raíz actual (ProyectoIntegrador1) a sys.path
     sys.path.insert(0, os.path.dirname(path))
-pp.include_router(login_router)
-app.include_router(doc_router)
-app.include_router(us_router)
+
+def main():
+    load_dotenv()
+    config = {
+        "user": os.getenv('DB_USER'),
+        "password": os.getenv('DB_PASS'),
+        "host": os.getenv("DB_HOST"),
+        "port": int(os.getenv("DB_PORT")),
+        "database": os.getenv('DB_NAME'),
+        "cursorclass": pymysql.cursors.DictCursor
+    }
+    db = MySqlManager(config)
+    print(config)
+    try:
+        db.open_db()
+        print("Hay conexion a base de datos")
+    except Exception as e:
+        print(f"Error critico: {e}")
+
+
 
 if __name__ == '__main__':
+    main()
