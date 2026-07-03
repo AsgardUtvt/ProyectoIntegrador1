c5ed3d95cb0cc1d76cff00691a4c964253b7fca0
Asgard <al222510064@gmail.com>
2026-07-02 22:08:04 -0700

    Cambios en directoiros y ficheros

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import uvicorn
-from fastapi import FastAPI
 from Login.LoginController import router as login_router
 from Documentacion.EjemploApi import router as doc_router
 from Usuario.UsuarioController import router as us_router
@@ -11,13 +9,8 @@ if __package__ is None and not getattr(sys, 'frozen', False):
     path = os.path.realpath(os.path.abspath(__file__))
     # Agrega la carpeta raíz actual (ProyectoIntegrador1) a sys.path
     sys.path.insert(0, os.path.dirname(path))
-app = FastAPI()
-app.include_router(login_router)
+pp.include_router(login_router)
 app.include_router(doc_router)
 app.include_router(us_router)
 
 if __name__ == '__main__':
-    HOST = "127.0.0.1"
-    PORT = 8000
-    RELOAD = True
-    uvicorn.run("main:app", host=HOST, port=PORT, reload=RELOAD)
