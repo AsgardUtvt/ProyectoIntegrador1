96810531a445fb58760451068a82aa8cde667879
AsgardUtvt <al222510064@gmail.com>
2026-07-21 23:47:19 -0600

    Add: Se agrego clase limitador de caracteres

--- Consultorio/ventana_crear_consultorio.py
+++ Consultorio/ventana_crear_consultorio.py
@@ -4,6 +4,7 @@ from PyQt6 import uic
 from Consultorio.Model.consultorio_model import Consultorio_Model
 from Consultorio.Servicio.estado_servicio import Estado_Servicio
 from message_box import Message_Box
+from limitar_intput import Limitar_Intput
 
 class Vetana_Crear_Consultorio(QWidget):
 
@@ -15,6 +16,9 @@ class Vetana_Crear_Consultorio(QWidget):
         self.mb = Message_Box()
         self.es = Estado_Servicio()
         self.cb_estado.addItems(self.llenar_cbx_estado())
+        self.le_nombre_consultorio.setValidator(Limitar_Intput.limitar_caracteres("regular"))
+
+
 
     def llenar_cbx_estado(self) -> list:
         try:
@@ -28,3 +32,5 @@ class Vetana_Crear_Consultorio(QWidget):
             self.navegar.ir_a_ventan("login")
             return []
 
+    def btn_crear_usuario(self):
+        pass
