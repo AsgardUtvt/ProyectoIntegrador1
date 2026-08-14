5677c162a3ab9a8a21d93ab397e7a6c6ad3aa872
AsgardUtvt <al222510064@gmail.com>
2026-08-14 04:15:05 -0600

    onsultorio con delete y generar nuevo

--- Documentacion/script_sql/sihmed.sql
+++ Documentacion/script_sql/sihmed.sql
@@ -959,7 +959,11 @@ CREATE TABLE `Sub_Consultorio` (
   KEY `FK_SUB_CONSULTORIO_ID_ESTADO` (`id_estado`),
   CONSTRAINT `FK_SUB_CONSULTORIO_CONSULTORIO` FOREIGN KEY (`id_consultorio`) REFERENCES `Consultorio` (`id_consultorio`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_SUB_CONSULTORIO_ID_ESTADO` FOREIGN KEY (`id_estado`) REFERENCES `Estado` (`id_estado`)
+<<<<<<< HEAD
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+=======
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+>>>>>>> eda639e (Consultorio con delete y generar nuevo)
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -969,9 +973,12 @@ CREATE TABLE `Sub_Consultorio` (
 SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
 LOCK TABLES `Sub_Consultorio` WRITE;
 /*!40000 ALTER TABLE `Sub_Consultorio` DISABLE KEYS */;
+<<<<<<< HEAD
 INSERT INTO `Sub_Consultorio` VALUES
 (1,'SubClinicaUno','arstarstarst','sraastarst','s','s','No encontrado','1235123532152','1235123512351',1,1,'12345','holamundo'),
 (2,'SubConsultoiroDos','arstarstrst','arstarstars','ss','ss','arstqw341234','1235123124351','1235123412341',1,1,'12312','arstarstarst');
+=======
+>>>>>>> eda639e (Consultorio con delete y generar nuevo)
 /*!40000 ALTER TABLE `Sub_Consultorio` ENABLE KEYS */;
 UNLOCK TABLES;
 COMMIT;
@@ -1319,4 +1326,8 @@ SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
+<<<<<<< HEAD
 -- Dump completed on 2026-08-14  4:55:56
+=======
+-- Dump completed on 2026-08-13 22:40:13
+>>>>>>> eda639e (Consultorio con delete y generar nuevo)
